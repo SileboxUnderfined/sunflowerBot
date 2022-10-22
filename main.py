5d651eda9094da723c29141cb75a0218050954a6
SileboxUnderfined <valerkafm@gmail.com>
2022-10-23 00:45:19 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from os import environ as envv
+from vk_api import VkApi, VkUpload
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST','GET'])
+def index(): pass
+
+@app.route(envv['BOT_ADDRESS'], methods=['POST'])
+def bot():
+    data = request.get_json(force=True,silent=True)
+
+    return 'ok'
+
+if __name__ in "__main__":
+    botSession = VkApi(token=envv['VK_API_KEY'])
+    bs = botSession.get_api()
+    vupl = VkUpload(bs)
+
+    app.run(host='0.0.0.0',port=envv['PORT'],debug=True)
